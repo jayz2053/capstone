569b293b6ac657acf71e6ab3b98af12db2a4d281
jshinde <jzs3@students.uwf.edu>
2018-03-21 12:21:37 -0500

    Updated the flask with main GETs

--- app.py
+++ app.py
@@ -63,6 +63,72 @@ class Office(db.Model):
 	start = db.Column
 	end = db.Column
 	
+class TeachesSchema(ma.Schema):
+	class Meta:
+		#Fields to expose
+		fields = ('crn')
+		
+teaches_schema = TeachesSchema()
+teaches_schema = TeachesSchema(many=true)
+		
+class ProfessorSchema(ma.Schema):
+	class Meta:
+		#Fields to expose
+		fields = ('name', 'email')
+		
+professor_schema = ProfessorSchema()
+professor_schema = ProfessorSchema(many=true)
+		
+class DestinationSchema(ma.Schema):
+	class Meta:
+		#Fields to expose
+		fields = ('category', 'dest_ID')
+		
+destination_schema = DestinationSchema()
+destination_schema = DestinationSchema(many=true)		
+		
+class CourseSchema(ma.Schema):
+	class Meta:
+		#Fields to expose
+		fields = ('dest_ID', 'days', 'times')
+		
+course_schema = CourseSchema()
+course_schema = CourseSchema(many=true)
+		
+class OfficeSchema(ma.Schema):
+	class Meta:
+		#Fields to expose
+		fields = ('dest_ID', 'email', 'day', 'start', 'end')
+		
+office_schema = OfficeSchema()
+office_schema = OfficeSchema(many=true)
+
+@app.route("/teaches", methods=["GET"])
+def get_teaches():
+	all_crn = Teaches.query.all()
+	result = teaches_schema.dump(all_crn)
+	return jsonify(result.data)
 	
-
-
+@app.route("/professor", methods=["GET"])
+def get_professor():
+	all_professor = Professor.query.all()
+	result = professor_schema.dump(all_professor)
+	return jsonify(result.data)
+	
+@app.route("/destination", methods=["GET"])
+def get_destination():
+	all_destination = Destination.query.all()
+	result = destination_schema.dump(all_destination)
+	return jsonify(result.data)
+	
+@app.route("/course", methods=["GET"])
+def get_course():
+	all_course = Course.query.all()
+	result = course_schema.dump(all_course)
+	return jsonify(result.data)
+	
+@app.route("/office", methods=["GET"])
+def get_office():
+	all_office = Office.query.all()
+	result = office_schema.dump(all_office)
+	return jsonify(result.data)
